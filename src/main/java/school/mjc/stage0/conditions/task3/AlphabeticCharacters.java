3cd575143438a21fa6080a716484b68247473a71
guilt <gulilty4all@gmail.com>
2022-08-18 17:21:28 +0600

    solution

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String alphabetEng = new String("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        String engVovels = new String("aeiouyAEIOUY");
+        if (alphabetEng.indexOf(character) >= 0 && alphabetEng.indexOf(character) <= (alphabetEng.length() - 1)){
+            if (engVovels.indexOf(character) >= 0 && engVovels.indexOf(character) <= (alphabetEng.length() - 1)){
+                System.out.println("Vowel");
+            }else{
+                System.out.println("Consonant");
+            }
+        }else{
+            System.out.println("wrong alphabet!");
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        String alphabetEng = new String("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        if (alphabetEng.indexOf(symbol) >= 0 && alphabetEng.indexOf(symbol) <= (alphabetEng.length() - 1)){
+            System.out.println("English");
+        }else{
+            System.out.println("Non English");
+        }
     }
 }
