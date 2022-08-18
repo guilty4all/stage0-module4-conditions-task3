e2433a0a383359308ee9557feb243e3a91699fec
guilt <gulilty4all@gmail.com>
2022-08-18 17:30:10 +0600

    second solution

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,12 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
-        if(month != 2 && month != 8 && month % 2 == 0){
-            System.out.println("30");
-        }else if (month == 2){
-            System.out.println("28");
-        } else{
-            System.out.println("31");
-        }
+       if(month > 0 && month <= 12){
+           if(month != 2 && month != 8 && month % 2 == 0){
+               System.out.println("30");
+           }else if (month == 2){
+               System.out.println("28");
+           } else {
+               System.out.println("31");
+           }
+       }else{
+           System.out.println("wrong number!");
+       }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,12 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        float result ;
         if(salary <= 10000){
-            System.out.println(salary - (salary/100)*15);
+            result = salary - (salary/100)*15;
+            System.out.println(result);
         } else if (salary < 10000 || salary <= 20000) {
-            System.out.println(salary - (salary/100)*18);
+            result = salary - (salary/100)*18;
+            System.out.println(result);
         } else if (salary > 20000) {
-            System.out.println(salary - (salary/100)*20);
+            result = salary - (salary/100)*20;
+            System.out.println(result);
         } else if (salary < 0) {
             System.out.println("wrong input!");
         }
