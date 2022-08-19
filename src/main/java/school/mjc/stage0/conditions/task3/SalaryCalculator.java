7b1d41c942e1fbee34e0bd500e354b4c242fb18b
guilt <gulilty4all@gmail.com>
2022-08-19 10:17:42 +0600

    final solution

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,16 +3,20 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         float result ;
-        if(salary <= 10000){
-            result = salary - (salary/100)*15;
-            System.out.println(result);
-        } else if (salary < 10000 || salary <= 20000) {
-            result = salary - (salary/100)*18;
-            System.out.println(result);
-        } else if (salary > 20000) {
-            result = salary - (salary/100)*20;
-            System.out.println(result);
-        } else if (salary < 0 || salary == -1.0) {
+        if(salary > 0){
+            if(salary <= 10000){
+                result = salary - (salary/100)*15;
+                System.out.println(result);
+            } else if (salary < 10000 || salary <= 20000) {
+                result = salary - (salary/100)*18;
+                System.out.println(result);
+            } else if (salary > 20000) {
+                result = salary - (salary/100)*20;
+                System.out.println(result);
+            } else {
+                System.out.println("wrong input!");
+            }
+        }else{
             System.out.println("wrong input!");
         }
     }
